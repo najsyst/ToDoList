cf5bc800b6184b270321445d2f05350a04c8a20d
Bartłomiej <bartlomiejpielaszek@gmail.com>
2023-05-08 23:18:31 +0200

    Add functionality to 'Add New List' button

---
+++ script.js
@@ -0,0 +1,30 @@
+const newListButton = document.getElementById("add-list-button");
+const list = document.getElementById("Lists-list");
+
+
+function addNewList(){
+    newListButton.addEventListener('click', ()=>{
+        const newButtonItem = document.createElement("button");
+        const newListItem = document.createElement("li");
+
+        newButtonItem.classList.add("transparent");
+        newListItem.appendChild(newButtonItem);
+
+        const lastItem = list.children[list.children.length - 1];
+        const secondLastItem = list.children[list.children.length - 2];
+        const secondLastItemText = secondLastItem.textContent;
+        const lastCharacter = secondLastItemText.charAt(secondLastItemText.length - 1);
+
+        if (!isNaN(lastCharacter)) {
+            let lastInt = parseInt(lastCharacter);
+            const newInt = lastInt + 1;
+            newButtonItem.textContent = "List" + newInt;
+        }
+        else {
+            newButtonItem.textContent = "List1";
+        }
+        list.insertBefore(newListItem, lastItem);
+    })
+}
+
+addNewList();
